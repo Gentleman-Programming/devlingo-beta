1520f0d9a160ef08cde507a4f4cd9a83739ddd48
Kevin Dev <kevin.developer71@gmail.com>
2022-03-21 17:29:05 -0600

    firebase auth service

---
+++ src/services/firebase/firebase.service.ts
@@ -0,0 +1,48 @@
+// Import the functions you need from the SDKs you need
+import { User } from '@/models';
+import {
+  createUserWithEmailAndPassword,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut
+} from 'firebase/auth';
+import { auth } from './firebase.config';
+
+/**
+ * @desc it calls the signup endpoint if the form is valid.
+ * @param  User
+ * @return Promise<UserCredentials>
+ */
+export const signup = async ({ email, password }: User) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+/**
+ * @desc it open a popup that allow to signin with google.
+ * @return Promise<UserCredentials>
+ */
+export const signinWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
+  // This gives you a Google Access Token. You can use it to access the Google API.
+  // return GoogleAuthProvider.credentialFromResult(result);
+};
+
+/**
+ * @desc it open a popup that allow to signin with github.
+ * @return Promise<UserCredentials>
+ */
+export const signinWithGithub = async () => {
+  const provider = new GithubAuthProvider();
+  return signInWithPopup(auth, provider);
+  // This gives you a GitHub Access Token. You can use it to access the Google API.
+  // const credential = GithubAuthProvider.credentialFromResult(result);
+  // The signed-in user info.
+};
+
+// TODO
+export const login = ({ email, password }: User) => signInWithEmailAndPassword(auth, email, password);
+// TODO
+export const logout = () => signOut(auth);
---
+++ src/services/firebase/index.ts
@@ -0,0 +1,2 @@
+export * from './firebase.config';
+export * from './firebase.service';
