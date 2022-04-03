562fafa3674049ecdc3f08d49f0bfd9455f35bee
José Berríos <surphury@gmail.com>
2022-04-02 21:28:34 -0400

    feat: migrate to react v18.0.0

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-
-ReactDOM.render(
-  <React.StrictMode>
+import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </StrictMode>
+);
