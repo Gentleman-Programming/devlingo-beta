ef2c77ed7129122a09f617e0cda5258c9b5a49f5
José Berríos <surphury@gmail.com>
2022-04-08 20:11:53 -0400

    fix: ts generate error

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+
+if (container instanceof HTMLElement) {
+  const root = createRoot(container);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
