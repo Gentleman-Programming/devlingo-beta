370b94315d795b3a537f49694ab2f017bdfe6b04
Jose Acosta <jdap.dmc@gmail.com>
2022-04-11 15:31:52 -0400

    fix: replace px and em by rem

--- src/styled-components/Button.styled.components.tsx
+++ src/styled-components/Button.styled.components.tsx
@@ -2,11 +2,11 @@ import styled from 'styled-components';
 
 export const Button = styled.button`
   background-color: #ea1889;
-  border-radius: 50px;
+  border-radius: 3rem;
   border: none;
   color: #ffffff;
-  width: 300px;
-  height: 3em;
+  width: 18rem;
+  height: 5rem;
   cursor: pointer;
   box-sizing: border-box;
   transition: background-color 0.3s ease;
@@ -16,7 +16,7 @@ export const Button = styled.button`
   &:hover {
     background-color: #181818;
     color: #ea1889;
-    border: 5px solid #ea1889;
+    border: 0.4rem solid #ea1889;
   }
 `;
 
