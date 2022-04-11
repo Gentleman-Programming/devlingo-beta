0cc7a6049166f476c777ffece0d74a9b817429de
Jose Acosta <jdap.dmc@gmail.com>
2022-04-11 15:37:44 -0400

    fix: delete position

--- src/styled-components/Mustachi.styled.components.tsx
+++ src/styled-components/Mustachi.styled.components.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Mustachi = styled.img`
   width: 70%;
   height: 25%;
-  position: relative;
-  left: 40px;
+  margin-right: auto;
+  margin-left: auto;
 `;
 
 export default Mustachi;
