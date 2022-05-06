2370f0f5c0e9189a58ec16ddf5bf7582073faa61
Jose Acosta <jdap.dmc@gmail.com>
2022-05-06 09:05:18 -0400

    fix: ignore state in create user

--- src/redux/states/user.tsx
+++ src/redux/states/user.tsx
@@ -1,25 +1,24 @@
 import { FirebaseUser } from '@/models';
-import { setAndPersistDbUserState } from '@/services';
 import { createSlice } from '@reduxjs/toolkit';
 
 export const UserEmptyState: FirebaseUser = {
   uid: '',
   email: '',
-  token: ''
+  accessToken: '',
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState: UserEmptyState,
   reducers: {
-    createUser: (state, action) => {
+    createUser: (_state, action) => {
       return action.payload;
     },
     modifyUser: (state, action) => {
       return { ...state, ...action.payload };
     },
-    resetUser: () => UserEmptyState
-  }
+    resetUser: () => UserEmptyState,
+  },
 });
 
 // Action creators are generated for each case reducer function
