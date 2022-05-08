42bd5dc22437d7aae739cf0f88493e60291fe5fc
Jose Acosta <jdap.dmc@gmail.com>
2022-05-08 10:08:02 -0400

    refactor(user): persist localStorage utility

--- src/redux/states/user.tsx
+++ src/redux/states/user.tsx
@@ -1,6 +1,9 @@
 import { FirebaseUser } from '@/models';
 import { createSlice } from '@reduxjs/toolkit';
 
+import { persistDataLocalStorage } from '@/utilities';
+import { localStorageEntities } from '@/models';
+
 export const UserEmptyState: FirebaseUser = {
   uid: '',
   email: '',
@@ -9,19 +12,30 @@ export const UserEmptyState: FirebaseUser = {
 
 export const userSlice = createSlice({
   name: 'user',
-  initialState: localStorage.getItem('user_info') ? JSON.parse(localStorage.getItem('user_info') as string) : UserEmptyState,
+  initialState: localStorage.getItem(localStorageEntities.user)
+    ? JSON.parse(localStorage.getItem(localStorageEntities.user) as string)
+    : UserEmptyState,
   reducers: {
     createUser: (_state, action) => {
-      localStorage.setItem('user_info', JSON.stringify(action.payload));
+      persistDataLocalStorage({
+        data: action.payload,
+        entity: localStorageEntities.user,
+      });
       return action.payload;
     },
     modifyUser: (state, action) => {
       const editDataUser = { ...state, ...action.payload };
-      localStorage.setItem('user_info', JSON.stringify(editDataUser));
+      persistDataLocalStorage({
+        data: editDataUser,
+        entity: localStorageEntities.user,
+      });
       return editDataUser;
     },
     resetUser: () => {
-      localStorage.setItem('user_info', JSON.stringify(UserEmptyState));
+      persistDataLocalStorage({
+        data: UserEmptyState,
+        entity: localStorageEntities.user,
+      });
       return UserEmptyState;
     },
   },
