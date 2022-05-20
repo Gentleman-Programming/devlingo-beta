71437072d4356e5940ce0db5cad1f8c0541068ef
joer9514 <joer9514@gmail.com>
2022-05-20 13:48:58 -0500

    [Add] findAllTech has been added.

--- src/services/firebase/firebase.question.ts
+++ src/services/firebase/firebase.question.ts
@@ -9,16 +9,37 @@ export const addCollection = async (techName: string, path: seniority, data: IQu
 
     return await Promise.all([
       setDoc(doc(dbRef, techName), { Title: techName }),
-      addDoc(collectionRef, { seniority: collectionRef.id, techName, ...data }),
+      addDoc(collectionRef, {
+        seniority: collectionRef.id,
+        techName, ...data
+      }),
     ]);
   } catch (error) {
     return error;
   }
 };
 
-export const viewCollection = async (techName: string, seniorityName: seniority) => {
+export const viewCollection = async (seniorityName: seniority) => {
   try {
-    const q = query(collection(db, 'question', techName, seniorityName));
+    const findTech = await findOneTech();
+    let data = <any>[];
+
+    for(let { Title } of findTech){
+      const q = query(collection(db, 'question', Title, seniorityName));
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => data.push({ id: doc.id, ...doc.data() }));
+    }    
+
+    return data;
+
+  } catch (error) {
+    return error;
+  }
+};
+
+export const findOneTech = async () => {
+  try {
+    const q = query(collection(db, 'question'));
     const querySnapshot = await getDocs(q);
     let data = <any>[];
 
@@ -29,6 +50,23 @@ export const viewCollection = async (techName: string, seniorityName: seniority)
     return error;
   }
 };
+
+export const findAllTech = async () => {
+  try {
+    const TR = viewCollection(seniority.TR);
+    const JR = viewCollection(seniority.JR);
+    const SSR = viewCollection(seniority.SSR);
+    const SR = viewCollection(seniority.SR);
+    const tech: IQuestion[] = [];
+
+    const questions = await Promise.all([TR, JR, SSR, SR]);
+    questions.map((elem: IQuestion[]) => tech.push(...elem));
+
+    return tech;
+  } catch (error) {
+    return error;    
+  }
+}
 
 export const updateCollection = async () => {};
 export const removeCollection = async () => {};
