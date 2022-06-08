d290afe7b11fa7800f057abc869cae6d6f8764aa
Erik Giovani <46170949+ErikGIovani@users.noreply.github.com>
2022-06-08 15:24:28 -0500

    Dropdown Develop

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,28 @@
-import { NavbarL } from '../../styled-components'
+import { NavbarL } from '../../styled-components';
+import { Link } from 'react-router-dom'; 
+import { useRef } from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Layout = ({children}: any) => {
+    const buttonStylesRef = useRef<any>();
+
+    const handleClass = () => {
+        if (buttonStylesRef.current) {
+            buttonStylesRef.current.classList.toggle('active');
+        }
+    };
     return (
         <>
             <NavbarL>
-                <ArrowBackIosIcon sx={{ fontSize: '5vmin' }} />
-                <AccountCircleIcon sx={{ fontSize: '7vmin' }} />
+                <ArrowBackIosIcon sx={{ fontSize: '4vmax' }} />
+                <div className="dropdown">
+                    <AccountCircleIcon onClick={handleClass}  sx={{ fontSize: '6vmax' }} />
+                    <div className="dropdown-content" ref={buttonStylesRef}>
+                        <Link to="/home">Perfil</Link>
+                        <Link to="/home">Cerrar Sesion</Link>
+                    </div>
+                </div>
             </NavbarL>
             <main>
                 {children}
