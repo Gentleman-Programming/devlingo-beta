4d52bacf81d2068beeea1b1321d8a775c2cb183d
Kevin Dev <kevin.developer71@gmail.com>
2022-06-09 12:22:01 -0600

    se agrega el acento en la o de la palabra sesion

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,34 +1,32 @@
 import { NavbarL } from '../../styled-components';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
 import { useRef } from 'react';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
-const Layout = ({children}: any) => {
-    const buttonStylesRef = useRef<any>();
+const Layout = ({ children }: any) => {
+  const buttonStylesRef = useRef<any>();
 
-    const handleClass = () => {
-        if (buttonStylesRef.current) {
-            buttonStylesRef.current.classList.toggle('active');
-        }
-    };
-    return (
-        <>
-            <NavbarL>
-                <ArrowBackIosIcon sx={{ fontSize: '4vmax' }} />
-                <div className="dropdown">
-                    <AccountCircleIcon onClick={handleClass}  sx={{ fontSize: '6vmax' }} />
-                    <div className="dropdown-content" ref={buttonStylesRef}>
-                        <Link to="/home">Perfil</Link>
-                        <Link to="/home">Cerrar Sesion</Link>
-                    </div>
-                </div>
-            </NavbarL>
-            <main>
-                {children}
-            </main>
-        </>
-    );
+  const handleClass = () => {
+    if (buttonStylesRef.current) {
+      buttonStylesRef.current.classList.toggle('active');
+    }
+  };
+  return (
+    <>
+      <NavbarL>
+        <ArrowBackIosIcon sx={{ fontSize: '4vmax' }} />
+        <div className="dropdown">
+          <AccountCircleIcon onClick={handleClass} sx={{ fontSize: '6vmax' }} />
+          <div className="dropdown-content" ref={buttonStylesRef}>
+            <Link to="/home">Perfil</Link>
+            <Link to="/home">Cerrar Sesión</Link>
+          </div>
+        </div>
+      </NavbarL>
+      <main>{children}</main>
+    </>
+  );
 };
 
 export default Layout;
