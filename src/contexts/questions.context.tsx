4dca4a8dfeb8f01f262f755c96ac3dc22071094d
José Berríos <surphury@gmail.com>
2022-07-23 08:51:47 -0400

    fix: use reducer

--- src/contexts/questions.context.tsx
+++ src/contexts/questions.context.tsx
@@ -1,6 +1,6 @@
-import { createContext, useEffect, useReducer, ReactNode, useState } from 'react';
+import { createContext, useReducer } from 'react';
 
-import { questionReducer /* actionTypes */ } from '@/reducers';
+import { questionReducer, actionTypes } from '../reducers';
 import { getDataLocalStorage } from '@/utilities';
 import { IQuestion, localStorageEntities } from '@/models';
 
@@ -14,50 +14,31 @@ type questionContextProps = {
 export const QuestionsContext = createContext<questionContextProps>({} as questionContextProps);
 
 interface props {
-  children: ReactNode;
+  children: JSX.Element | JSX.Element[];
 }
 
 export const QuestionProvider = ({ children }: props) => {
-  const [questions, setQuestions] = useState<IQuestion[]>(getDataLocalStorage<IQuestion[]>(localStorageEntities.questions));
+  const questions = getDataLocalStorage<IQuestion[]>(localStorageEntities.questions);
 
-  const [state, setState] = useState(questions.reduce((acum, { point }) => acum + point, 0));
+  const state = questions.reduce((acum, { point }) => acum + point, 0);
 
-  const [seniority, setSeniority] = useState(state);
+  const [seniority, dispatch] = useReducer(questionReducer, state);
 
   const IncrementSeniority = (points: number): void => {
-    setSeniority(updater(true, state, points));
-    /*  dispatch({
-		type: actionTypes.increment,
-		payload: points,
-    }); */
+    dispatch({
+      type: actionTypes.increment,
+      payload: points,
+    });
   };
 
   const DecrementSeniority = (points: number): void => {
-    setSeniority(updater(false, state, points));
-    /* dispatch({
+    dispatch({
       type: actionTypes.reduce,
       payload: points,
-    }); */
+    });
   };
 
   const value = { IncrementSeniority, DecrementSeniority, seniority, initialState: state };
 
-  console.log(value);
   return <QuestionsContext.Provider value={value}>{children}</QuestionsContext.Provider>;
-};
-
-const updater = (increment = false, state: number, points: number): number => {
-  if (!increment) {
-    if (state > 0) {
-      const df = state - points;
-      if (df > 0) return df;
-      return 0;
-    }
-
-    return state;
-  } else if (increment) {
-    return state + points;
-  } else {
-    return state;
-  }
 };
