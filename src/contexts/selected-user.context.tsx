dc67c1a4793605aa489dff450daaf71dfb515903
Jose Acosta <jdap.dmc@gmail.com>
2022-09-08 15:51:24 -0400

    fix: white window question 1

--- src/contexts/selected-user.context.tsx
+++
@@ -1,7 +0,0 @@
-import { User } from '@/models';
-import { createContext } from 'react';
-
-export const SelectedUserContext = createContext({
-  selectedUser: {} as User,
-  setSelectedUser: (user: User) => {}
-});
